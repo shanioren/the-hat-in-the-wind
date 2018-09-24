663f8a9b566b0c5fe5330c6e9d60a934d191964b
shanioren <shanioren100@gmail.com>
2018-09-24 23:08:18 +0300

    remove unused code

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
   render() {
     return (
       <ThemeProvider theme={Theme}>
-        <div className="App">
+        <div>
           <MtWashingtonHatInTheWindContainer />
         </div>
       </ThemeProvider>
